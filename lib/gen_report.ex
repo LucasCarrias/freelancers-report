16bc94695a650366a028a78a03e94e232ab0bc63
Lucas Carrias <lucascarrias@outlook.com>
2022-02-05 16:09:46 -0300

    count hours per month

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,21 @@
 defmodule GenReport do
   alias GenReport.Parser
 
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
@@ -9,20 +24,27 @@ defmodule GenReport do
 
   defp generate(list) do
     %{
-      all_hours: process_all_hours(list)
+      all_hours: process_all_hours(list),
+      hours_per_month: process_hours_per_month(list)
     }
   end
 
-  defp all_names(list) do
+  defp get_all_names(list) do
     list
     |> Enum.map(&List.first/1)
     |> Enum.uniq()
   end
 
+  defp build_all_months() do
+    Enum.into(@months, %{}, &{&1, 0})
+  end
+
   # All Hours
 
   defp build_all_hours(list) do
-    Enum.into(all_names(list), %{}, &{&1, 0})
+    list
+    |> get_all_names()
+    |> Enum.into(%{}, &{&1, 0})
   end
 
   defp sum_all_hours(line, report) do
@@ -33,5 +55,22 @@ defmodule GenReport do
   defp process_all_hours(list) do
     report = build_all_hours(list)
     Enum.reduce(list, report, fn line, acc -> sum_all_hours(line, acc) end)
+  end
+
+  # Hours per month
+
+  defp build_hours_per_month(list) do
+    Enum.into(get_all_names(list), %{}, &{&1, build_all_months()})
+  end
+
+  defp sum_hours_per_month(line, report) do
+    [name, hours, _, month, _] = line
+    worker_months = Map.put(report[name], month, report[name][month] + hours)
+    Map.put(report, name, worker_months)
+  end
+
+  defp process_hours_per_month(list) do
+    report = build_hours_per_month(list)
+    Enum.reduce(list, report, fn line, acc -> sum_hours_per_month(line, acc) end)
   end
 end
